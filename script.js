fd18262f8cd182aaf13f4cd344956e939c9af5c2
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-03-05 08:22:36 +0500

    Refactoring code

--- script.js
+++ script.js
@@ -18,48 +18,17 @@ function toggleNav() {
   
   overlay.classList.toggle('overlay-active');
   if (overlay.classList.contains('overlay-active')) {
-    // overlay.classList.remove('overlay-slide-left')
-    // overlay.classList.add('overlay-slide-right')
     overlay.classList.replace('overlay-slide-left', 'overlay-slide-right')
-
-    // nav1.classList.remove('slide-out-1')
-    // nav1.classList.add('slide-in-1')
-    // nav2.classList.remove('slide-out-2')
-    // nav2.classList.add('slide-in-2')
-    // nav3.classList.remove('slide-out-3')
-    // nav3.classList.add('slide-in-3')
-    // nav4.classList.remove('slide-out-4')
-    // nav4.classList.add('slide-in-4')
-    // nav5.classList.remove('slide-out-5')
-    // nav5.classList.add('slide-in-5')
 
     navAnimation('out', 'in');
   } else {
-    // overlay.classList.remove('overlay-slide-right')
-    // overlay.classList.add('overlay-slide-left')
     overlay.classList.replace('overlay-slide-right', 'overlay-slide-left');
 
-    // nav1.classList.remove('slide-in-1')
-    // nav1.classList.add('slide-out-1')
-    // nav2.classList.remove('slide-in-2')
-    // nav2.classList.add('slide-out-2')
-    // nav3.classList.remove('slide-in-3')
-    // nav3.classList.add('slide-out-3')
-    // nav4.classList.remove('slide-in-4')
-    // nav4.classList.add('slide-out-4')
-    // nav5.classList.remove('slide-in-5')
-    // nav5.classList.add('slide-out-5')
     navAnimation('in', 'out');
   }
 }
 
-
 menuBars.addEventListener('click', toggleNav);
-// nav1.addEventListener('click', toggleNav)
-// nav2.addEventListener('click', toggleNav)
-// nav3.addEventListener('click', toggleNav)
-// nav4.addEventListener('click', toggleNav)
-// nav5.addEventListener('click', toggleNav)
 navItems.forEach(nav => {
   nav.addEventListener('click', toggleNav);
 });
